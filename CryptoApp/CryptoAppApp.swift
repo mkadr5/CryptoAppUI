47b0d471c4e7d3dbd741f328670a82f2e6ee65b2
mkadr5 <muhammetkadir52@gmail.com>
2023-04-02 20:57:07 +0300

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Muhammet Kadir on 2.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
